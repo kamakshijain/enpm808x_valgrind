eef262b72b06cf20dbc297e5deadf8b3fb39ad73
kamakshijain <kamakshi@terpmail.umd.edu>
2019-10-06 17:52:08 -0400

    Added test

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,15 @@
+/**
+ *  @file    ../test/test.icpp
+ *  @author  Kamakshi Jain
+ *  @copyright Kamakshi Jain
+ *  @brief Valgrind exercise enpm808x
+ */
 #include <gtest/gtest.h>
-
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+#include"AnalogSensor.hpp"
+/**
+*@brief Test checks if the Read() method outputs the right value or not.
+*/
+AnalogSensor test(10);
+TEST(AnalogSensorTest, Read_Test) {
+  EXPECT_EQ(10, test.Read());
 }
